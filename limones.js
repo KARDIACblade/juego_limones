a4a491d407dd0f8eef00b8b5fb1d525e108ae2a0
KARDIACblade <bladescorpio@gmail.com>
2026-09-12 21:43:04 -0500

    mover personaje a la izquierda

--- limones.js
+++ limones.js
@@ -6,6 +6,8 @@ const   ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 
+let personajeX = canvas.width/2
+
 function iniciar (){
     dibujarSuelo();
     dibujarPersonaje();
@@ -20,5 +22,19 @@ function dibujarSuelo (){
 
 function dibujarPersonaje (){
     ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO  +ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO  +ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE)
+}
+
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarCanva();
+}
+
+ function actualizarCanva(){
+         limpiarCanva();
+         dibujarSuelo();
+         dibujarPersonaje();
+ }
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
